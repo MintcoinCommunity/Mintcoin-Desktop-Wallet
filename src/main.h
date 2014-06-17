67b82e96efe91ef40584e2d93a3bc90c48ae6429
presstab <presstab1337@gmail.com>
2014-06-17 08:49:14 -0600

    Add FastStart

--- src/main.h
+++ src/main.h
@@ -85,6 +85,7 @@ extern std::map<uint256, CBlock*> mapOrphanBlocks;
 
 // Settings
 extern int64 nTransactionFee;
+extern bool fUseFastIndex;
 
 // Minimum disk space required - used in CheckDiskSpace()
 static const uint64 nMinDiskSpace = 52428800;
@@ -1352,6 +1353,9 @@ public:
 /** Used to marshal pointers into hashes for db storage. */
 class CDiskBlockIndex : public CBlockIndex
 {
+private:
+	uint256 blockHash;
+	
 public:
     uint256 hashPrev;
     uint256 hashNext;
@@ -1360,6 +1364,7 @@ public:
     {
         hashPrev = 0;
         hashNext = 0;
+		blockHash = 0;
     }
 
     explicit CDiskBlockIndex(CBlockIndex* pindex) : CBlockIndex(*pindex)
@@ -1401,10 +1406,14 @@ public:
         READWRITE(nTime);
         READWRITE(nBits);
         READWRITE(nNonce);
+		READWRITE(blockHash);
     )
 
     uint256 GetBlockHash() const
     {
+		if (fUseFastIndex && (nTime < GetAdjustedTime() - 12 * nMaxClockDrift) && blockHash != 0)
+			return blockHash;
+			
         CBlock block;
         block.nVersion        = nVersion;
         block.hashPrevBlock   = hashPrev;
@@ -1412,9 +1421,11 @@ public:
         block.nTime           = nTime;
         block.nBits           = nBits;
         block.nNonce          = nNonce;
-        return block.GetHash();
-    }
-
+    
+		const_cast<CDiskBlockIndex*>(this)->blockHash = block.GetHash();
+		
+		return blockHash;
+	}
 
     std::string ToString() const
     {
