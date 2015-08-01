1952e4259cd5ffad6ffe40349cc7ddcf76ad025f
Press Tab <presstab1337@gmail.com>
2015-08-01 14:47:45 -0600

    bump build to 2.0.0.2

--- src/version.h
+++ src/version.h
@@ -49,6 +49,6 @@ static const int MEMPOOL_GD_VERSION = 60002;
 #define DISPLAY_VERSION_MAJOR       2
 #define DISPLAY_VERSION_MINOR       0
 #define DISPLAY_VERSION_REVISION    0
-#define DISPLAY_VERSION_BUILD       0
+#define DISPLAY_VERSION_BUILD       1
 
 #endif
