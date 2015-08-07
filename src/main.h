bf3dbd4b57814458dc4ff08543e9316e74450f30
supasonic <supasonic128@gmail.com>
2015-08-06 21:47:09 -0500

    Compact serialization for variable-length integers
    BTC Commit(s):
     69fc8047a9a96bcf5360f810c796049c27e16fcd
    4d6144f97faf9d2a6c89f41d7d2360f21f0b71e2

--- src/main.h
+++ src/main.h
@@ -726,7 +726,18 @@ protected:
     const CTxOut& GetOutputFor(const CTxIn& input, const MapPrevTx& inputs) const;
 };
 
-
+/** wrapper fo CTxOut that provides a more compact serialization */
+class CTxOutCompressor
+{
+private:
+    CTxOut & txout;
+public:
+    CTxOutCompressor(CTxOut &txoutIn) : txout(txoutIn){}
+    IMPLEMENT_SERIALIZE(
+        READWRITE(VARINT(txout.nValue));
+        CScriptCompressor cscript(REF(txout.scriptPubKey));
+        READWRITE(cscript);)
+};
 
 
 
