84612681c763dc33a7102fee736e26d3b161ce79
supasonic <supasonic128@gmail.com>
2015-10-12 16:53:49 -0500

    Convert fRescan argument to importprivkey to bool
    BTC Commits
    5dfbd4ff68c68c0565311576cf7e04a3d5c8f8e5
    a8fbbd8fb0b29f3d852936e913a94d6d21baebd8

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -1244,6 +1244,7 @@ Array RPCConvertValues(const std::string &strMethod, const std::vector<std::stri
     if (strMethod == "sendalert"              && n > 6) ConvertTo<boost::int64_t>(params[6]);
     if (strMethod == "lockunspent"            && n > 0) ConvertTo<bool>(params[0]);
     if (strMethod == "lockunspent"            && n > 1) ConvertTo<Array>(params[1]);
+    if (strMethod == "importprivkey"          && n > 2) ConvertTo<bool>(params[2]);
 
     return params;
 }
