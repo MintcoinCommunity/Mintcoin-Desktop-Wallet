8e0916f2f61d70cb3bf55c2a6fb7b47115d0e59b
supasonic <supasonic128@gmail.com>
2015-10-13 23:27:38 -0500

    Add const versions of base_uint.end()/begin(), make size() const.
    BTC Commit(s):
    68feac96b67b80a4fe59c4f349b58af9c77e2709

--- src/uint256.h
+++ src/uint256.h
@@ -335,16 +335,26 @@ public:
     }
 
     unsigned char* begin()
-    const {
+    {
         return (unsigned char*)&pn[0];
     }
 
     unsigned char* end()
-    const {
+    {
         return (unsigned char*)&pn[WIDTH];
     }
 
-    unsigned int size()
+    const unsigned char* begin() const
+    {
+        return (unsigned char*)&pn[0];
+    }
+
+    const unsigned char* end() const
+    {
+        return (unsigned char*)&pn[WIDTH];
+    }
+
+    unsigned int size() const
     {
         return sizeof(pn);
     }
