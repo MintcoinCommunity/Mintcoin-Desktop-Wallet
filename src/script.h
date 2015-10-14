4897549873d005d91db8ca61bdca6dd09cb35c63
supasonic <supasonic128@gmail.com>
2015-10-14 12:52:34 -0500

    Send transactions after a CMerkleBlock when asked for it in an inv.
    This actually simplifies some SPV code, as they can keep track of
    a filtered block and its txn before accepting both in one step.
    The previous argument was that SPV nodes should handle the txn the
    same as any other free txn and then mark them as connected to a
    block when they get the filtered block itself.  However, it now
    appears that SPV nodes will need to put in more effort to verify
    loose txn than they would to verify txn in blocks, thus making it
    more approriate to send the txn after the filtered block.
    
    
    Replace 520 constant with MAX_SCRIPT_ELEMENT_SIZE
    
    BTC Commit(s):
    28b80e6065fb3ada78a6ea49faf2912beb59ca27
    192cc910ec7cade1d0dce7f3b111e7fc7720e607

--- src/script.h
+++ src/script.h
@@ -19,6 +19,8 @@ typedef std::vector<unsigned char> valtype;
 class CCoins;
 class CTransaction;
 
+static const unsigned int MAX_SCRIPT_ELEMENT_SIZE = 520; // bytes
+
 /** Signature hash types/flags */
 enum
 {
