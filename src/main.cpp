2d53508780e9cf47f54e22375261c70f661bd334
supasonic <supasonic128@gmail.com>
2015-10-22 12:52:29 -0500

    Limited mapAlreadyAskedFor
    Move mapAlreadyAskedFor to limitedmap
    
    This will result in re-requesting invs if we are under heavy inv
    load, however as long as we get no more than 16,000 invs in two
    minutes, this should have no effect on runtime behavior.
    
    Add a limitedmap class similar to mruset 
    
    
    Revert "Actually use mapAlreadyAskedFor."
    
    This reverts commit 643160f.
    
    Turns out this commit was useless after a more careful reading of
    CNode::AskFor
    
    BTC Commit(s):
    b5afda67f2846ddc6554304acc1567796733725b
    5ffc299404f4bcce3b9b2522096f8ed53d3181d5
    eb59c4ca8e8be07c587f1cbcb252153b97f08dcc

--- src/main.cpp
+++ src/main.cpp
@@ -4505,7 +4505,6 @@ bool SendMessages(CNode* pto, bool fSendTrickle)
                     pto->PushMessage("getdata", vGetData);
                     vGetData.clear();
                 }
-                mapAlreadyAskedFor[inv] = nNow;
             }
             pto->mapAskFor.erase(pto->mapAskFor.begin());
         }
