783ab55dbdc8abae97535dfd2e1944747584bcdf
supasonic <supasonic128@gmail.com>
2015-10-23 11:36:23 -0500

    CRPCCommand.unlocked -> CRPCCommand.threadSafe
    BTC Commit(s):
    10ef3611697cdf867730689f6968ba48adc4d6e2

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -193,8 +193,8 @@ Value stop(const Array& params, bool fHelp)
 
 
 static const CRPCCommand vRPCCommands[] =
-{ //  name                      actor (function)         okSafeMode unlocked
-  //  ------------------------  -----------------------  ---------- --------
+{ //  name                      actor (function)         okSafeMode threadSafe
+  //  ------------------------  -----------------------  ---------- ----------
     { "help",                   &help,                   true,      true },
     { "stop",                   &stop,                   true,      true },
     { "getblockcount",          &getblockcount,          true,      false },
@@ -1031,7 +1031,7 @@ json_spirit::Value CRPCTable::execute(const std::string &strMethod, const json_s
         // Execute
         Value result;
         {
-            if (pcmd->unlocked)
+            if (pcmd->threadSafe)
                 result = pcmd->actor(params, false);
             else {
                 LOCK2(cs_main, pwalletMain->cs_wallet);
--- src/bitcoinrpc.h
+++ src/bitcoinrpc.h
@@ -97,7 +97,7 @@ public:
     std::string name;
     rpcfn_type actor;
     bool okSafeMode;
-    bool unlocked;
+    bool threadSafe;
 };
 
 /**
