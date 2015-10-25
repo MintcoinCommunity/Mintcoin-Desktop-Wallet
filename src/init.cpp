817a4f286d8c7289b86f81854a96270b14098986
supasonic <supasonic128@gmail.com>
2015-10-25 14:50:49 -0500

    Minor fixes
    Don't attempt to resize vector to negative size.
    
    Fix crash-at-shutdown if exiting before initializing wallet
    
    Explicitly #include stdarg.h, for access to va_list
    
    Explictly cast calculation to int, to allow std::max to work.
    
    BTC Commit(s):
    98ab2b5a265e4794d6a9d22212d992bfd1f9c1f1
    dbc6dea1b27e87bca0ef496b8b74d6f027bb7de3
    65ec9eab2bf1aea0bd580e1d972f54163b58b332
    03f498080a813850e1a3addf2c555450aa5e65c1

--- src/init.cpp
+++ src/init.cpp
@@ -101,7 +101,8 @@ void Shutdown()
     StopNode();
     {
         LOCK(cs_main);
-        pwalletMain->SetBestChain(CBlockLocator(pindexBest));
+        if (pwalletMain)
+            pwalletMain->SetBestChain(CBlockLocator(pindexBest));
         if (pblocktree)
             pblocktree->Flush();
         if (pcoinsTip)
@@ -531,7 +532,7 @@ bool AppInit2(boost::thread_group& threadGroup)
     // Make sure enough file descriptors are available
     int nBind = std::max((int)mapArgs.count("-bind"), 1);
     nMaxConnections = GetArg("-maxconnections", 125);
-    nMaxConnections = std::max(std::min(nMaxConnections, FD_SETSIZE - nBind - MIN_CORE_FILEDESCRIPTORS), 0);
+    nMaxConnections = std::max(std::min(nMaxConnections, (int)(FD_SETSIZE - nBind - MIN_CORE_FILEDESCRIPTORS)), 0);
     int nFD = RaiseFileDescriptorLimit(nMaxConnections + MIN_CORE_FILEDESCRIPTORS);
     if (nFD < MIN_CORE_FILEDESCRIPTORS)
         return InitError(_("Not enough file descriptors available."));
