2dc884a9f0f12999690d02cbded21eb841055b5a
supasonic <supasonic128@gmail.com>
2015-10-28 20:09:07 -0500

    Move pMiningKey init out of StartRPCThreads
    This commit decouples the pMiningKey initialization and shutdown from the RPC
    threads.
    
    `getwork` and `getblocktemplate` rely on pMiningKey, and can also be ran
    from the debug window in the UI even when the RPC server is not running.
    
    Solves issue #2706.
    
    BTC Commit(s):
    d98bf10f23e0e633ff2ff33075a353d30bf862b4

--- src/init.cpp
+++ src/init.cpp
@@ -98,6 +98,7 @@ void Shutdown()
     RenameThread("bitcoin-shutoff");
     nTransactionsUpdated++;
     StopRPCThreads();
+    ShutdownRPCMining();
     bitdb.Flush(false);
     StopNode();
     {
@@ -1086,6 +1087,8 @@ bool AppInit2(boost::thread_group& threadGroup)
 
     StartNode(threadGroup);
 
+    // InitRPCMining is needed here so getwork/getblocktemplate in the GUI debug console works properly.
+    InitRPCMining();
     if (fServer)
         StartRPCThreads();
 
