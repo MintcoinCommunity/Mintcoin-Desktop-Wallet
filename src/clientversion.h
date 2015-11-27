ba2ecadc082d25cc17e7fcbb07fa31d1d2e6667f
supasonic <supasonic128@gmail.com>
2015-11-27 11:56:04 -0600

    Initial Autotools build system
    BTC Commit(s):
    f286aa1611c625e16fae7af83bd10f5607272c9d
    1e9d3b1392b604fafc6802e4d43469f519d14424
    9a09c0091c33a8f22f48b5e6e5b8f3b400c43ff3
    56ba02aa2a0bb56cbdfeb87ec47c0c5b61f9bb06
    b4d8d03b86405193e79310231a2d7ce3b636d554
    941dba17838a1a40f5a5862eb2f84ffec511911e
    35b8af92265ed74de63c3818e5290c27b3f35df2
    f80b723c6689773a23600b5a866a10f56565d2b7
    12564aa1664cb81b3a81e1a706bde997108aba00
    efdfee423f23df88ffa4b846e4522419fb081ce1

--- src/clientversion.h
+++ src/clientversion.h
@@ -1,15 +1,18 @@
 #ifndef CLIENTVERSION_H
 #define CLIENTVERSION_H
 
+#if defined(HAVE_CONFIG_H)
+#include "bitcoin-config.h"
+#else
 //
 // client versioning and copyright year
 //
 
 // These need to be macros, as version.cpp's and bitcoin-qt.rc's voodoo requires it
-#define CLIENT_VERSION_MAJOR       0
-#define CLIENT_VERSION_MINOR       7
-#define CLIENT_VERSION_REVISION    2
-#define CLIENT_VERSION_BUILD       0
+#define CLIENT_VERSION_MAJOR       2
+#define CLIENT_VERSION_MINOR       1
+#define CLIENT_VERSION_REVISION    0
+#define CLIENT_VERSION_BUILD       1000
 
 // Set to true for release, false for prerelease or test build
 #define CLIENT_VERSION_IS_RELEASE  false
@@ -17,6 +20,8 @@
 // Copyright year (2009-this)
 // Todo: update this when changing our copyright comments in the source
 #define COPYRIGHT_YEAR 2015
+
+#endif //HAVE_CONFIG_H
 
 // Converts the parameter X to a string after macro replacement on X has been performed.
 // Don't merge these into one macro!
