cdebbecc6cbb833ec51de99f3a28efbc6a43730e
supasonic <supasonic128@gmail.com>
2015-12-05 17:30:37 -0600

    Add dumpwallet and importwallet RPCs
    dumpwallet: produce a dump of all keys in a wallet, in a format
    compatible with Bitcoin Wallet for Android and Multibit.
    
    importwallet: import such a dump
    
    
     Add ExtractAffectedKeys to script
    
    This function finds all keys affected by a particular output script,
    supporting everything ExtractDestinations supports (pay-to-pubkey,
    pay-to-pubkeyhash, multisig) and recurses into subscripts (P2SH).
    
    
    Add GetKeyBirthTimes to wallet
    
    Compute safe lower bounds on the birth times of all wallet keys. For
    pool keys or keys with metadata, the actually stored birth time is
    used. For all others, the birth times are inferred from the wallet
    transactions.
    
    BTC Commit(s):
    4e534aa9d892dd6db64afda979ba1f8d44371389
    acc775c5547a94fa5ad12ecb0bdaeefdc285d853
    434e4273b96cb9204fea346c3fbc65583b01c55b

--- src/wallet.h
+++ src/wallet.h
@@ -181,6 +181,8 @@ public:
     bool ChangeWalletPassphrase(const SecureString& strOldWalletPassphrase, const SecureString& strNewWalletPassphrase);
     bool EncryptWallet(const SecureString& strWalletPassphrase);
 
+    void GetKeyBirthTimes(std::map<CKeyID, int64> &mapKeyBirth) const;
+
     /** Increment the next transaction order id
         @return next transaction order id
      */
@@ -227,7 +229,7 @@ public:
     void ReturnKey(int64 nIndex);
     bool GetKeyFromPool(CPubKey &key);
     int64 GetOldestKeyPoolTime();
-    void GetAllReserveKeys(std::set<CKeyID>& setAddress);
+    void GetAllReserveKeys(std::set<CKeyID>& setAddress) const;
 
     std::set< std::set<CTxDestination> > GetAddressGroupings();
     std::map<CTxDestination, int64> GetAddressBalances();
