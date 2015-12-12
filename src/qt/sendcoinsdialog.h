d88c62b2567387ec168e4b725d427d17af5f5070
supasonic <supasonic128@gmail.com>
2015-12-11 23:26:28 -0600

    allow emit message() in sendcoinsdialog and walletview
    - this allows us to use emit message() over MessageBox:: or gui->message()
      calls in sendcoinsdialog and walletview
    - move main handlePaymentRequest() functionality back to BitcoinGUI
    - move a showNormalIfMinimized() before gotoSendCoinsPage()
    
    BTC Commit(s):
    2384a2864b6a0b29eec6410057aefe1fd8e7e585

--- src/qt/sendcoinsdialog.h
+++ src/qt/sendcoinsdialog.h
@@ -71,6 +71,10 @@ private slots:
     void coinControlClipboardPriority();
     void coinControlClipboardLowOutput();
     void coinControlClipboardChange();
+
+signals:
+    // Fired when a message should be reported to the user
+    void message(const QString &title, const QString &message, unsigned int style);
 };
 
 #endif // SENDCOINSDIALOG_H
