4bdd43f8024cf8a23b8f07debdf7aab94e41d3bf
supasonic <supasonic128@gmail.com>
2016-01-24 13:59:18 -0600

    Bitcoin-Qt: add a Reset button to the options dialog
    - a click on "Reset Options" sets all options to the default values by
      removing all stored settings (QSettings), loading the defaults and
      saving them as the new settings
    - before the reset is executed the user is presented a confirmation dialog
    - special casing was needed for StartAtStartup
    
    BTC Commits
    5fb445b49e80812f004f00d5adf8fdd39bec557f

--- src/qt/optionsmodel.h
+++ src/qt/optionsmodel.h
@@ -42,6 +42,7 @@ public:
     };
 
     void Init();
+    void Reset();
 
     /* Migrate settings from wallet.dat after app initialization */
     bool Upgrade(); /* returns true if settings upgraded */
