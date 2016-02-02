bc807324a346e71594321aa24dd4de23b2902a9f
supasonic <supasonic128@gmail.com>
2016-02-02 00:26:52 -0600

    Fix off-by-one errors in use of IsFinalTx()
    Previously CreateNewBlock() didn't take into account the fact that
    IsFinalTx() without any arguments tests if the transaction is considered
    final in the *current* block, when both those functions really needed to
    know if the transaction would be final in the *next* block.
    
    Additionally the UI had a similar misunderstanding.
    
    Also adds some basic tests to check that CreateNewBlock() is in fact
    mining nLockTime-using transactions correctly.
    
    Thanks to Wladimir J. van der Laan for rebase.
    
    BTC Commits
    665bdd3bc9ba4ac566edf5ba3fa8bbd93eb4780f

--- src/miner.cpp
+++ src/miner.cpp
@@ -192,7 +192,7 @@ CBlock* CreateNewBlock(CWallet* pwallet, bool fProofOfStake)
         for (map<uint256, CTransaction>::iterator mi = mempool.mapTx.begin(); mi != mempool.mapTx.end(); ++mi)
         {
             CTransaction& tx = (*mi).second;
-            if (tx.IsCoinBase() || tx.IsCoinStake() || !IsFinalTx(tx))
+            if (tx.IsCoinBase() || tx.IsCoinStake() || !IsFinalTx(tx, pindexPrev->nHeight + 1))
                 continue;
 
             COrphan* porphan = NULL;
