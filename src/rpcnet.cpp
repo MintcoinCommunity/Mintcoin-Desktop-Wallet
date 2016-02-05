a726451b77102ab6cdc16119be8db8cce97d8dff
supasonic <supasonic128@gmail.com>
2016-02-05 17:47:00 -0600

    Document new `walletpassphrase` behavior in 0.9
    Also add a note to the release notes.
    Fixes #3672.
    
    always show syncnode in getpeerinfo (fixes #2717) 
    
    BTC Commit(s):
    3e0753b0f5a0d2b2d8ed647439bcb8bb8370629f
    86648a8d16699ab392508a48bb867d3fc4f7d6cf
    3e0753b0f5a0d2b2d8ed647439bcb8bb8370629f

--- src/rpcnet.cpp
+++ src/rpcnet.cpp
@@ -127,6 +127,8 @@ Value getpeerinfo(const Array& params, bool fHelp)
         obj.push_back(Pair("services", strprintf("%08"PRIx64, stats.nServices)));
         obj.push_back(Pair("lastsend", (boost::int64_t)stats.nLastSend));
         obj.push_back(Pair("lastrecv", (boost::int64_t)stats.nLastRecv));
+        obj.push_back(Pair("bytessent", (boost::int64_t)stats.nSendBytes));
+        obj.push_back(Pair("bytesrecv", (boost::int64_t)stats.nRecvBytes));
         obj.push_back(Pair("conntime", (boost::int64_t)stats.nTimeConnected));
         obj.push_back(Pair("pingtime", stats.dPingTime));
         if (stats.dPingWait > 0.0)
@@ -141,6 +143,7 @@ Value getpeerinfo(const Array& params, bool fHelp)
         if (fStateStats) {
             obj.push_back(Pair("banscore", statestats.nMisbehavior));
         }
+        obj.push_back(Pair("syncnode", stats.fSyncNode));
 
         ret.push_back(obj);
     }
