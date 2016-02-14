c237fa645c79a6e6ebe3dd9a3f479c7e1ae4afce
supasonic <supasonic128@gmail.com>
2016-02-14 03:37:11 -0600

    [Qt] Dont set table color in coin control
    BTC Commit(s):
    b51700a852f4220601f6f47ddc51725b59b2da42

--- src/qt/coincontroldialog.cpp
+++ src/qt/coincontroldialog.cpp
@@ -17,7 +17,6 @@
 #include <QApplication>
 #include <QCheckBox>
 #include <QClipboard>
-#include <QColor>
 #include <QCursor>
 #include <QDialogButtonBox>
 #include <QFlags>
@@ -657,9 +656,6 @@ void CoinControlDialog::updateView()
 
             itemWalletAddress->setFlags(flgTristate);
             itemWalletAddress->setCheckState(COLUMN_CHECKBOX,Qt::Unchecked);
-
-            for (int i = 0; i < ui->treeWidget->columnCount(); i++)
-                itemWalletAddress->setBackground(i, QColor(248, 247, 246));
 
             // label
             itemWalletAddress->setText(COLUMN_LABEL, sWalletLabel);
