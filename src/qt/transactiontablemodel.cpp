4f7661031738c5073b88e6ec4d0cf10827239e31
supasonic <supasonic128@gmail.com>
2016-02-17 20:28:01 -0600

    Locking robustness fixes
    qt: get required locks upfront in polling functions
    
    This avoids the GUI from getting stuck on
    periodical polls if the core is holding the locks for a longer time -
    for example, during a wallet rescan.
    
    
    Add missing AssertLockHeld in ConnectBlock
    
    BTC Commit(s):
    41106a50d2be9358ab19e75c1d6d075a773525b7
    b39a07dc42ab6ba746a25206969fb81913146f1f

--- src/qt/transactiontablemodel.cpp
+++ src/qt/transactiontablemodel.cpp
@@ -24,7 +24,6 @@
 #include <QDebug>
 #include <QIcon>
 #include <QList>
-#include <QTimer>
 
 // Amount column is right-aligned it contains numbers
 static int column_alignments[] = {
@@ -187,13 +186,19 @@ public:
         {
             TransactionRecord *rec = &cachedWallet[idx];
 
+            // Get required locks upfront. This avoids the GUI from getting
+            // stuck if the core is holding the locks for a longer time - for
+            // example, during a wallet rescan.
+            //
             // If a status update is needed (blocks came in since last check),
             //  update the status of this transaction from the wallet. Otherwise,
             // simply re-use the cached status.
-            if(rec->statusUpdateNeeded())
+            TRY_LOCK(cs_main, lockMain);
+            if(lockMain)
             {
+                TRY_LOCK(wallet->cs_wallet, lockWallet);
+                if(lockWallet && rec->statusUpdateNeeded())
                 {
-                    LOCK(wallet->cs_wallet);
                     std::map<uint256, CWalletTx>::iterator mi = wallet->mapWallet.find(rec->hash);
 
                     if(mi != wallet->mapWallet.end())
--- src/qt/walletmodel.cpp
+++ src/qt/walletmodel.cpp
@@ -100,10 +100,21 @@ void WalletModel::updateStatus()
 
 void WalletModel::pollBalanceChanged()
 {
+    // Get required locks upfront. This avoids the GUI from getting stuck on
+    // periodical polls if the core is holding the locks for a longer time -
+    // for example, during a wallet rescan.
+    TRY_LOCK(cs_main, lockMain);
+    if(!lockMain)
+        return;
+    TRY_LOCK(wallet->cs_wallet, lockWallet);
+    if(!lockWallet)
+        return;
+
     if(chainActive.Height() != cachedNumBlocks)
     {
         // Balance and number of transactions might have changed
         cachedNumBlocks = chainActive.Height();
+
         checkBalanceChanged();
         if(transactionTableModel)
             transactionTableModel->updateConfirmations();
