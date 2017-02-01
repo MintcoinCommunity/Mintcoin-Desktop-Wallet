a79f1866aa10550814b8c880323e608c378fad2c
Fuzzbawls <fuzzbawls@gmail.com>
2017-01-31 17:19:50 -0800

    Update copyright headers

--- src/qt/sendcoinsentry.h
+++ src/qt/sendcoinsentry.h
@@ -1,5 +1,6 @@
-// Copyright (c) 2011-2013 The Bitcoin developers
-// Distributed under the MIT/X11 software license, see the accompanying
+// Copyright (c) 2011-2017 The Bitcoin Core developers
+// Copyright (c) 2014-2017 The MintCoin Developers
+// Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef BITCOIN_QT_SENDCOINSENTRY_H
