175c7807029f937b24cedef585912ad10818d779
Shane Kerr <shane@time-travellers.org>
2018-04-15 21:06:08 +0000

    Fiddling with build

--- src/mxe-build.sh
+++ src/mxe-build.sh
@@ -2,10 +2,10 @@
 
 if [ $1 == "windows32" ]; then
     MXE_TARGET="i686-w64-mingw32.static"
-    CPU_TARGET="i686"
+#    CPU_TARGET="i686"
 else
     MXE_TARGET="x86-64-w64-mingw32.static"
-    CPU_TARGET="x86_64"
+#    CPU_TARGET="x86_64"
 fi
 
 sudo apt-get update
@@ -23,9 +23,9 @@ sudo apt-get --yes install mxe-${MXE_TARGET}-db
 sudo apt-get --yes install mxe-${MXE_TARGET}-boost
 sudo apt-get --yes install mxe-${MXE_TARGET}-miniupnpc
 
-NCPU=`cat /proc/cpuinfo | grep -c ^processor`
-
-MXEDIR=/usr/lib/mxe
-export PATH=$PATH:$MXEDIR/usr/bin
-make -f makefile.linux-mingw -j $NCPU \
-    DEPSDIR=$MXEDIR/usr/$MXE_TARGET TARGET_PLATFORM=$CPU_TARGET
+#NCPU=`cat /proc/cpuinfo | grep -c ^processor`
+#
+#MXEDIR=/usr/lib/mxe
+#export PATH=$PATH:$MXEDIR/usr/bin
+#make -f makefile.linux-mingw -j $NCPU \
+#    DEPSDIR=$MXEDIR/usr/$MXE_TARGET TARGET_PLATFORM=$CPU_TARGET
