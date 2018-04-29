31e4f1476fcc1fea059934436332d684aa3ed293
Shane Kerr <shane@time-travellers.org>
2018-04-29 22:31:15 +0000

    Script to push

---
+++ src/mega-push.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+# This script pushes the binaries built to MEGA. This is useful for
+# sending the result of a Travis CI build to a site where we can
+# convert it into a package for release.
+
+# Syntax:
+#   bash mega-push.sh [exe_name] [name_on_mega]
+
+# Exit on error
+set -e
+
+# Install MEGAcmd
+if [ $TRAVIS_OS_NAME = linux ]; then
+    if [ `lsb_release -is` -eq 'Ubuntu' ]; then
+        if [ `lsb_release -cs` -eq 'trusty' ]; then
+             PKG="megacmd-xUbuntu_14.04_amd64.deb"
+             PKG_URL="https://mega.nz/linux/MEGAsync/xUbuntu_14.04/amd64/$PKG"
+        fi
+        wget $PKG_URL
+        # package installation will fail because of dependencies - that's ok
+        sudo dpkg -i $PKG || true
+        sudo apt-get -y -f install
+    fi
+fi
+
+mega-login $MEGA_EMAIL $MEGA_PASSWORD
+mega-put $*
