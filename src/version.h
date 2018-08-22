75ab8dde10e25278e9a9a42c07b905a2cc3dcd12
Shane Kerr <shane@time-travellers.org>
2018-08-22 23:06:14 +0200

    Set our versions and suchlike for 2.1-beta (#81)
    Getting versions and such ready for release

--- src/version.h
+++ src/version.h
@@ -46,9 +46,4 @@ static const int BIP0031_VERSION = 60000;
 // "mempool" command, enhanced "getdata" behavior starts with this version:
 static const int MEMPOOL_GD_VERSION = 60002;
 
-#define DISPLAY_VERSION_MAJOR       2
-#define DISPLAY_VERSION_MINOR       0
-#define DISPLAY_VERSION_REVISION    3
-#define DISPLAY_VERSION_BUILD       99
-
 #endif
