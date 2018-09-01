933dda5cd6948fc36ca15050efebb58f0c69d0a0
Shane Kerr <shane@time-travellers.org>
2018-09-01 21:33:01 +0200

    Update for 2.1.0 release

--- share/genbuild.sh
+++ share/genbuild.sh
@@ -13,7 +13,7 @@ else
     exit 1
 fi
 
-DESC="v2.1.0-beta1"
+DESC="v2.1.0"
 SUFFIX=""
 LAST_COMMIT_DATE=""
 if [ -e "$(which git 2>/dev/null)" -a "$(git rev-parse --is-inside-work-tree 2>/dev/null)" = "true" ]; then
