29e86cb211208b76e04d8de5081eac2ff848989d
Shane Kerr <shane@time-travellers.org>
2019-03-15 09:16:12 +0100

    Fix key of repository

--- src/mxe-build.sh
+++ src/mxe-build.sh
@@ -34,9 +34,10 @@ sudo apt-get update
 #     https://github.com/mxe/mxe/issues/2253
 
 #echo "deb http://pkg.mxe.cc/repos/apt/debian wheezy main" \
+#sudo apt-key adv --keyserver keyserver.ubuntu.com \
+#    --recv-keys D43A795B73B16ABE9643FE1AFD8FFF16DB45C6AB
 
-sudo apt-key adv --keyserver keyserver.ubuntu.com \
-    --recv-keys D43A795B73B16ABE9643FE1AFD8FFF16DB45C6AB
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 84C7C89FC632241A6999ED0A580873F586B72ED9
 sudo add-apt-repository 'deb [arch=amd64] http://mirror.mxe.cc/repos/apt trusty main'
 
 sudo apt-get update
